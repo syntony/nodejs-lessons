a2383e7a7a304d605f54b7b1367e475973ca97c6
anthony <anthony.synenko@gmail.com>
2018-06-19 12:00:10 +0300

    Express project with static pages ready for deploy on heroku

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,18 +1,46 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
+//  dynamic port for heroku
+const port = process.env.PORT || 3000;
+//  connect express
 const app = express();
-
+//  register partials for handlebars view engine
 hbs.registerPartials(__dirname + '/views/partials');
+//  set handlebars view engine as a view rngine for our node js app
 app.set('view engine', 'hbs');
+
+//  middlewares
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', `${log}\n`, err => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+//  static path
 app.use(express.static(__dirname + '/public'));
 
+//  helpers - you can use it in any place of hbs file
 hbs.registerHelper('getCurrentYear', () => (
   new Date().getFullYear()
 ));
 
-hbs.registerHelper('sceamIt', text => text.toUpperCase())
+hbs.registerHelper('sceamIt', text => text.toUpperCase());
 
+//  http routes
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
@@ -26,6 +54,16 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  });
 });
+
+//  listen port
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
+//  offtop
+//  for comfort development:
+//  nodemon server.js -e js,hbs
