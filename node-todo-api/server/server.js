8d3cb6f35d06c450146c6a34f5e37db8fc60e9f3
syntony <anthony.synenko@gmail.com>
2018-06-27 15:55:06 +0300

    Add GET /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -16,8 +17,8 @@ app.post('/todos', (req, res) => {
 
   todo.save().then(doc => {
     res.send(doc);
-  }, ({ message }) => {
-    res.status(400).send(message);
+  }, e => {
+    res.status(400).send(e);
   })
 });
 
@@ -25,9 +26,29 @@ app.get('/todos', (req, res) => {
   Todo.find()
     .then(todos => {
       res.send({ todos })
-    }, ({ message }) => {
-      res.status(400).send(message);
+    }, e => {
+      res.status(400).send(e);
     })
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send()
+    });
 });
 
 app.listen(3000, () => {
